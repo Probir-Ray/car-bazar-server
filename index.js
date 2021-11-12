865181663f7d9c8b09aef2910c670463f8646491
Probir-Ray <somironroy.need@gmail.com>
2021-11-12 10:53:54 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express();
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xrfjl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+console.log(uri);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Listening at ${port}`)
+})
