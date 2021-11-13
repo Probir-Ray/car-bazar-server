28d1afeeb88346d8a410c28d5c71ebc5339a0607
Probir-Ray <somironroy.need@gmail.com>
2021-11-13 19:35:49 +0600

    update

--- index.js
+++ index.js
@@ -18,12 +18,34 @@ async function run() {
         const database = client.db('car_bazar');
         const userCollection = database.collection('users');
 
+        app.get('/users/:email', async(req, res) => {
+          const email = req.params.email;
+          const query = {email};
+          const user = await userCollection.findOne(query);
+          let isAdmin = false;
+          if(user?.role === 'admin') {
+            isAdmin = true;
+          }
+          res.json({admin: isAdmin});
+        })
+
+        // Add new user to db
         app.post('/users', async(req, res) => {
           const user = req.body;
           const result = await userCollection.insertOne(user);
           res.json(result);
           console.log(result);
-        })
+        });
+
+        // Make admin method
+        app.put('/users/admin', async(req, res) => {
+          const user = req.body;
+          const filter = {email: user.email}
+          const updateDoc = {$set: {role: 'admin'}};
+          const result = await userCollection.updateOne(filter, updateDoc);
+          res.json(result);
+        });
+
     }
     finally {
 
