7ab8b115990ac59f9b616a07eb49c21b6a74dbd1
Probir-Ray <somironroy.need@gmail.com>
2021-11-13 16:59:27 +0600

    update

--- index.js
+++ index.js
@@ -6,12 +6,30 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xrfjl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-console.log(uri);
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('car_bazar');
+        const userCollection = database.collection('users');
+
+        app.post('/users', async(req, res) => {
+          const user = req.body;
+          const result = await userCollection.insertOne(user);
+          res.json(result);
+          console.log(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
